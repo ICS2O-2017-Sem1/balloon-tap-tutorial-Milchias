1fc92050a3c2282c6de478d2098ad23a06269d58
Milchias <Milchias.teare@ocsbstudent.ca>
2017-09-19 11:18:11 -0400

    Fisrt day

--- main.lua
+++ main.lua
@@ -3,5 +3,26 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+ local background = display.newImageRect( "background.png", 360, 570 )
+ background.x = display.contentCenterX
+ background.y = display.contentCenterY
 
--- Your code here
+local platform = display.newImageRect( "platform.png", 300, 50 )
+platform.x = display.contentCenterX
+platform.y = display.contentHeight-25
+balloon.alpha = 0.8
+
+local physics = require( "physics" )
+Physics.start()
+
+physics.addBody( platform, "static" )
+physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
+
+
+
+
+
+
+
+
+
